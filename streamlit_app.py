33ee3b9dccb867e0c0dedc68b3e968b1cd8bcc39
Jonas Jakiminski <28489507+Jakiminski@users.noreply.github.com>
2026-01-24 03:58:39 -0300

    fix InsertionSort colors

--- streamlit_app.py
+++ streamlit_app.py
@@ -281,13 +281,25 @@ class WebApp:
                 b_width = "2px"
 
                 if passo["alg"] == "Insertion":
-                    if i == passo["target_key_idx"]:
+                    if passo.get("finalizado"):
+                        bg = COLOR_FILL_INSERTION_SORT_SORTED
+                    else:
+                        if i <= passo["target_key_idx"]:
+                            bg = COLOR_FILL_INSERTION_SORT_SORTED
+                        if i == passo["curr_j"]:
+                            border = COLOR_BORDER_INSERTION_SORT_POINTER_J
+                            b_width = "4px"
+                        elif i == passo["target_key_idx"]:
+                            border = COLOR_BORDER_INSERTION_SORT_POINTER_KEY
+                            b_width = "4px"
+                    """if i == passo["target_key_idx"]:
                         border = COLOR_BORDER_INSERTION_SORT_ITERATOR_I 
                         b_width = "4px"
                     if i == passo["curr_j"]:
                         border = COLOR_BORDER_INSERTION_SORT_ITERATOR_J
                         b_width = "4px"
                         bg = COLOR_FILL_ARRAY_VISITED
+                    """
 
                 elif passo["alg"] == "Bubble":
                     if passo["action"] != "start" and passo["action"] != "finished":
@@ -527,6 +539,7 @@ class WebApp:
 # --- INICIALIZAÇÃO E EXECUÇÃO ---
 app = WebApp()
 app.render()
+
 
 
 
